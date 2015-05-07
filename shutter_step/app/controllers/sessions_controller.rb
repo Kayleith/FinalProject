00bfdbfb2faf251be4bf26fbda51f7a55d05cbb7
BrianKang <brian.john.kang@gmail.com>
2015-05-07 08:54:47 -0400

    first push

---
+++ shutter_step/app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+  before_action :require_signed_out!, only: [:new, :create]
+  before_action :require_signed_in!, only: [:destroy]
+
+  def new
+    @user = new User();
+    render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:name],
+      params[:password]
+    )
+
+    if @user
+      sign_in(@user)
+      redirect_to root_url
+    else
+      flash.now[:errors] = ["Invalid email or password."]
+      render :new
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to new_session_url
+  end
+end
